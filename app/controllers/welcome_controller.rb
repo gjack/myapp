0acc91becae260e420a77581386564360f629039
Gabi Jack <gabi@gabijack.com>
2022-04-01 17:26:54 -0400

    Use previous Redis host and port now that the cluster is gone

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    # redis = Redis.new(host: "redis", port: 6379)
-    redis = Redis.new(host: "db-redis-nyc1-92923-do-user-11220077-0.b.db.ondigitalocean.com", port: 25061)
+    redis = Redis.new(host: "redis", port: 6379)
 
     redis.incr "page hits"
 
