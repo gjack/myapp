3d2ef06f16fe40c90f28886683770bbce5926728
Gabi Jack <gabi@gabijack.com>
2022-04-01 11:51:39 -0400

    Use redis database added to digitalocean

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    redis = Redis.new(host: "redis", port: 6379)
+    # redis = Redis.new(host: "redis", port: 6379)
+    redis = Redis.new(host: "db-redis-nyc1-92923-do-user-11220077-0.b.db.ondigitalocean.com", port: 25061)
 
     redis.incr "page hits"
 
